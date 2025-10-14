8605121ef3005e6a5e0a1b0718eebeeeada32f1e
Lakshya-coder04 <Lakshyabirla7cr@gmail.com>
2025-10-14 08:02:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,23 +13,47 @@ load_dotenv()
 USERNAME= os.getenv("USERNAME")
 PASSWORD= os.getenv("PASSWORD")
 
+# instance of Service, Options that we imported 
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
-chrome_options.add_argument("--disable-autofill")
-chrome_options.add_argument("--disable-save-password-bubble")
-
-# instance of Service, Options that we imported 
 service = Service('chromedriver-win64/chromedriver.exe')
 driver = webdriver.Chrome(service=service)
+
+#load the webpage
 driver.get('https://demoqa.com/login')
 
+# getting username, password and login button
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+login_button = driver.find_element(By.ID, 'login')
 
-username_field.clear()
+# Fill in the username and password , click login
 username_field.send_keys("lba442")
 password_field.send_keys(PASSWORD)
+driver.execute_script("arguments[0].click();", login_button) #executing javascript code
 
+
+#locate the elements dropdown and text box 
+elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div')))
+
+elements.click()  
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
+
+# locate the form fields 
+fullname_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'email')))
+
+current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
+
+permanent_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentA')))
+
+login_button = driver.find_element(By.ID, 'login')
+
+# Fill in the form fields 
 
 input("PRESS ENTER TO CLOSE THE BROWSER")
 driver.quit()
